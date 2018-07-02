faba4530925eaef8fb6cfc6617a7d50680416412
oliverjohnsonwork <oliverjohnsonwork@gmail.com>
2018-07-02 15:26:10 +0300

    Repository addition.

--- src/main/java/com/multiheaded/webapp/repository/PersonRepository.java
+++ src/main/java/com/multiheaded/webapp/repository/PersonRepository.java
@@ -1,8 +1,10 @@
-package com.multiheaded.webapp.domain.repo;
+package com.multiheaded.webapp.repository;
 
 import com.multiheaded.webapp.domain.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // TODO перенести repo пакет в інше місце? Чи узагалі викинути в domain?
+@RepositoryRestResource
 public interface PersonRepository extends CrudRepository<Person, Long> {
 }
