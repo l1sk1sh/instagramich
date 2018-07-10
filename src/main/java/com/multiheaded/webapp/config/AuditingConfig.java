63ea35d64d2ef9f014da72d64bfb99af49c779d5
oliverjohnsonwork <oliverjohnsonwork@gmail.com>
2018-07-10 21:13:42 +0300

    Implementation of security. Broken!

---
+++ src/main/java/com/multiheaded/webapp/config/AuditingConfig.java
@@ -0,0 +1,10 @@
+package com.multiheaded.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+}
