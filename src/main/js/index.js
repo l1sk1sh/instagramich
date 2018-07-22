5545993ec9579e6efc3338fdc488c1f29c3091db
oliverjohnsonwork <oliverjohnsonwork@gmail.com>
2018-07-22 15:04:40 +0300

    F-E introduction

---
+++ src/main/js/index.js
@@ -0,0 +1,20 @@
+// TODO Introduce full live reload or separate dev and production development
+// TODO Move to Material ui
+// TODO Audit npm for vulnerabilities
+// TODO Fix arrow functions in SignUp (babel config)
+// TODO Separate messages for translation opportunities
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './assets/css/index.css';
+import App from './app/App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
+
+registerServiceWorker();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main/js/app.js',
+    entry: './src/main/js/index.js',
     devtool: 'sourcemaps',
     cache: true,
     output: {
@@ -11,15 +11,35 @@ module.exports = {
     },
     module: {
         rules: [
-            {
+            { //TODO Remove redundant rules
                 test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
+                exclude: [
+                    /(node_modules)/,
+                    /\.svg$/,
+                    'src/main/js/assets'
+                ],
                 loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
-                    presets: ['env', 'react']
+                    presets: ['react', 'env']
                 }
-            }
+            },
+            {
+                test: /(\.css$)|(\.less$)/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader?sourceMap" },
+                    { loader: 'less-loader', options: { javascriptEnabled: true } }
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: [{
+                    loader: 'babel-loader'
+                }, {
+                    loader: 'react-svg-loader'
+                }]
+            },
         ]
     }
 };
